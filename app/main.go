3860ef3dc8fd5dc714e57d69905ea0e717092761
Michael Schmitt <schmiddim@gmx.at>
2023-03-25 23:40:45 +0100

    Stage #2 done

--- app/main.go
+++ app/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -9,17 +10,29 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//fmt.Println("Logs from your program will appear here!")
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
-	}
 
-	fmt.Println(string(output))
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	writerStd := bufio.NewWriter(os.Stdout)
+	writerError := bufio.NewWriter(os.Stderr)
+	_, err = writerStd.ReadFrom(stdout)
+	_, err = writerError.ReadFrom(stderr)
+	if err != nil {
+		return
+	}
 }
